76606fa3ee8cefe92beb00f472d046ef1c5020de
Dmitro Zelinskyi <d.s.zelinsky@gmail.com>
2021-06-02 06:29:23 +0300

    Update alarm (now use NumberOfMessagesSent), run terraform fmt

--- alarm.tf
+++ alarm.tf
@@ -1,45 +1,17 @@
 resource "aws_cloudwatch_metric_alarm" "deadletter_queue_not_empty" {
-  alarm_name                = "${var.aws_sqs_queue_name}-deadletter-queue-non-empty"
-  comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "1"
-  threshold                 = "0"
-  alarm_description         = "${var.aws_sqs_queue_name} is not empty."
-  alarm_actions             = var.alarm_actions
+  alarm_name          = "${var.aws_sqs_queue_name}-deadletter-queue-non-empty"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  threshold           = "0"
+  alarm_description   = "${var.aws_sqs_queue_name} is not empty."
+  alarm_actions       = var.alarm_actions
 
-  metric_query {
-    id          = "e1"
-    expression  = "m1+m2"
-    label       = "Messages available"
-    return_data = "true"
-  }
-
-  metric_query {
-    id = "m1"
-
-    metric {
-      metric_name = "ApproximateNumberOfMessagesVisible"
-      namespace   = "AWS/SQS"
-      period      = "600"
-      stat        = "Sum"
-      unit        = "Count"
-      dimensions = {
-        QueueName = "${aws_sqs_queue.deadletter.name}"
-      }
-    }
-  }
-
-  metric_query {
-    id = "m2"
-
-    metric {
-      metric_name = "ApproximateNumberOfMessagesNotVisible"
-      namespace   = "AWS/SQS"
-      period      = "600"
-      stat        = "Sum"
-      unit        = "Count"
-      dimensions = {
-        QueueName = "${aws_sqs_queue.deadletter.name}"
-      }
-    }
+  metric_name = "NumberOfMessagesSent"
+  namespace   = "AWS/SQS"
+  period      = "300"
+  statistic   = "Sum"
+  unit        = "Count"
+  dimensions = {
+    QueueName = "${aws_sqs_queue.deadletter.name}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,75 +1,75 @@
-variable aws_sqs_queue_name {
-  type = string
+variable "aws_sqs_queue_name" {
+  type        = string
   description = "Name of the AWS SQS queue to create and to listen to."
 }
 
-variable aws_lambda_function_name {
-  type = string
+variable "aws_lambda_function_name" {
+  type        = string
   description = "Name of the AWS Lambda function to trigger."
 }
 
 
-variable visibility_timeout_seconds {
-  type = number
-  default = null
+variable "visibility_timeout_seconds" {
+  type        = number
+  default     = null
   description = "The visibility timeout for the queue. An integer from 0 to 43200 (12 hours). Default value will be calculated as 6 * timeout of provided AWS Lambda function."
 }
 
-variable delay_seconds {
-  type = number
-  default = 0
+variable "delay_seconds" {
+  type        = number
+  default     = 0
   description = "The time in seconds that the delivery of all messages in the queue will be delayed. An integer from 0 to 900 (15 minutes)."
 }
 
 
-variable batch_size {
-  type = number
-  default = 10
+variable "batch_size" {
+  type        = number
+  default     = 10
   description = "The largest number of records that Lambda will retrieve from your event source at the time of invocation, from 1 to 10,000."
 }
 
 
-variable maximum_batching_window_in_seconds {
-  type = number
-  default = 0
+variable "maximum_batching_window_in_seconds" {
+  type        = number
+  default     = 0
   description = "The maximum amount of time to gather records before invoking the function, in seconds (between 0 and 300). Records will continue to buffer (or accumulate in the case of an SQS queue event source) until either maximum_batching_window_in_seconds expires or batch_size has been met. For a batch size over 10, you must also set the maximum_batching_window_in_seconds parameter to at least 1 second."
 }
 
 
-variable enabled {
-  type = bool
-  default = true
+variable "enabled" {
+  type        = bool
+  default     = true
   description = "Determines if the mapping will be enabled on creation."
 }
 
 
-variable deadletter_max_receive_count {
-  type = number
-  default = 5
+variable "deadletter_max_receive_count" {
+  type        = number
+  default     = 5
   description = "Max number of retries which will happen before the message will be sent to Deadletter queue."
 }
 
 
-variable tags {
-  default = {}
+variable "tags" {
+  default     = {}
   description = "Tags to use for each of the created resources."
 }
 
 
-variable aws_lambda_function_iam_role_name {
-  type = string
+variable "aws_lambda_function_iam_role_name" {
+  type        = string
   description = "The name of IAM role attached to Lambda function."
 }
 
 
-variable aws_sqs_queue_policy {
+variable "aws_sqs_queue_policy" {
   description = "AWS SQS Queue Access Policy as a JSON string. Usable, say, to allow an S3 bucket to send notifications to this queue."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 
-variable alarm_actions {
-  type = list(string)
+variable "alarm_actions" {
+  type    = list(string)
   default = []
 }
