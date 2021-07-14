8bd3bf6a9b8752003c3f290d19c8929cb9d006f4
Aleksandr Zakharov <xezzed@gmail.com>
2021-07-15 02:48:41 +0300

    CBS-2716: .name => .function_name

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
 data "aws_lambda_function" "lambda" {
-  function_name = var.aws_lambda_function_to_trigger.name
+  function_name = var.aws_lambda_function_to_trigger.function_name
 
   depends_on = [
     var.aws_lambda_function_to_trigger
--- mapping.tf
+++ mapping.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_event_source_mapping" "this" {
   event_source_arn = aws_sqs_queue.main.arn
-  function_name    = var.aws_lambda_function_to_trigger.name
+  function_name    = var.aws_lambda_function_to_trigger.function_name
 
   batch_size                         = var.batch_size
   maximum_batching_window_in_seconds = var.maximum_batching_window_in_seconds
